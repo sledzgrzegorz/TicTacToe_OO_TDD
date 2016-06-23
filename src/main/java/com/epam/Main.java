20121492b61f487b08e0caf0c38b68db3030cf84
Grzegorz Śledź <grzegorzsledz91@gamil.com>
2016-06-23 20:00:13 +0200

    Setup completed. Ready for coding

---
+++ src/main/java/com/epam/Main.java
@@ -0,0 +1,11 @@
+package com.epam;
+
+/**
+ * Created by Grzesiek on 2016-06-23.
+ */
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("Test!");
+    }
+}
