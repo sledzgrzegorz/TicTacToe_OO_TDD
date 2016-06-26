de620a1759da9a2d0d163809b676d0959d826c0a
Grzegorz Sledz <grzegorz_sledz@epam.com>
2016-06-26 21:03:21 +0200

    Game logic was added. Now we can play using console

--- src/main/java/com/epam/tictactoe/gamelogic/TicTacToeGame.java
+++ src/main/java/com/epam/tictactoe/gamelogic/TicTacToeGame.java
@@ -1,10 +1,13 @@
 package com.epam.tictactoe.gamelogic;
 
-import com.epam.tictactoe.board.Board;
-import com.epam.tictactoe.board.IBoard;
-import com.epam.tictactoe.board.IDimension;
-import com.epam.tictactoe.board.Position;
+import com.epam.tictactoe.arbiter.Arbiter;
+import com.epam.tictactoe.arbiter.IArbiter;
+import com.epam.tictactoe.board.*;
 import com.epam.tictactoe.enums.Sign;
+import com.epam.tictactoe.input.ConsoleInput;
+import com.epam.tictactoe.input.IInput;
+import com.epam.tictactoe.player.IPlayer;
+import com.epam.tictactoe.player.Player;
 
 /**
  * Created by grzegorz_sledz on 24.06.16.
@@ -12,14 +15,63 @@ import com.epam.tictactoe.enums.Sign;
 public class TicTacToeGame {
     private final IBoard board;
     private final IDimension dimension;
+    private final IInput input;
+    private final IArbiter arbiter;
+    private final IPlayer playerO;
+    private final IPlayer playerX;
+    private IPlayer currentPlayer;
 
-    public TicTacToeGame(IDimension dimension){
-        this.dimension=dimension;
-        this.board=new Board(dimension);
+    public TicTacToeGame(IDimension dimension) {
+        this.dimension = dimension;
+        this.board = new Board(dimension);
+        this.input = new ConsoleInput();
+        this.arbiter = new Arbiter();
+        this.playerO = new Player(Sign.O);
+        this.playerX = new Player(Sign.X);
     }
-    void start(){
-        board.placeMark(new Position(5), Sign.O);
+
+    void start() {
+        currentPlayer = playerO;
+        IPosition position;
+        int turns = 0;
         System.out.println(board);
+        do {
+            doMove();
+            turns++;
+            System.out.println(board);
+            if (turns >= 5 && arbiter.isWinner(currentPlayer.winningSeqiences())) {
+                System.out.println("The winner is: " + currentPlayer);
+                stop();
+            }
+            nextPlayer();
+        } while (!board.isFullyFilled());
+        System.out.println("Nobody won");
+    }
+
+    public void doMove() {
+        boolean correctMove = false;
+        do {
+            System.out.println(currentPlayer + " choose field [1-" + dimension.getValue() * dimension.getValue() + "]: ");
+            IPosition position = new Position(input.read());
+            correctMove = board.placeMark(position, currentPlayer.getSign());
+            if (correctMove) {
+                currentPlayer.saveMove(position);
+            } else {
+                System.out.println(position + " is not valid. Try again!!");
+            }
+        } while (!correctMove);
+    }
+
+    public void nextPlayer() {
+        if (currentPlayer == playerO) {
+            this.currentPlayer = playerX;
+        } else {
+            this.currentPlayer = playerO;
+        }
+    }
+
+    public void stop() {
+        System.exit(0);
     }
 
 }
