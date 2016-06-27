5c8e70f35527ba0986c0a4ebb4988c4b629acf5a
Grzegorz Sledz <grzegorz_sledz@epam.com>
2016-06-27 11:53:13 +0200

    Second scenario was created

--- src/main/java/com/epam/tictactoe/gamelogic/TicTacToeGame.java
+++ src/main/java/com/epam/tictactoe/gamelogic/TicTacToeGame.java
@@ -5,6 +5,10 @@ import com.epam.tictactoe.board.IBoard;
 import com.epam.tictactoe.board.IDimension;
 import com.epam.tictactoe.board.Position;
 import com.epam.tictactoe.enums.Sign;
+import com.epam.tictactoe.player.IPlayer;
+import com.epam.tictactoe.player.IPlayerManager;
+import com.epam.tictactoe.player.Player;
+import com.epam.tictactoe.player.PlayerManager;
 
 /**
  * Created by grzegorz_sledz on 24.06.16.
@@ -12,13 +16,18 @@ import com.epam.tictactoe.enums.Sign;
 public class TicTacToeGame {
     private final IBoard board;
     private final IDimension dimension;
+    private final IPlayerManager playerManager;
 
     public TicTacToeGame(IDimension dimension){
         this.dimension=dimension;
         this.board=new Board(dimension);
+        this.playerManager=new PlayerManager(new Player(Sign.O),new Player(Sign.X));
     }
     void start(){
-        board.placeMark(new Position(5), Sign.O);
+        board.placeMark(new Position(5), playerManager.next().getSign());
+        board.placeMark(new Position(4), playerManager.next().getSign());
+        board.placeMark(new Position(3), playerManager.next().getSign());
+        board.placeMark(new Position(2), playerManager.next().getSign());
         System.out.println(board);
 
     }
